0df9096a33feef4e7dd4558350b1cd8be607c3bc
admin <admin@tapartnersllc.net>
2017-08-24 22:20:16 -0400

    1st 4 hours of work tests passed

--- src/main/java/com/mark/BabySitter.java
+++ src/main/java/com/mark/BabySitter.java
@@ -2,7 +2,8 @@ package com.mark;
 
 public class BabySitter {
     public String baby(Integer placeHolder) {
-        return String.valueOf(placeHolder * 12);
-
+        if (placeHolder <= 4) // baby awake until end of 1st 4 hours $12/hr
+            return String.valueOf(placeHolder * 12);
+        return null;
     }
 }
--- src/test/BabySitterTest.java
+++ src/test/BabySitterTest.java
@@ -9,4 +9,23 @@ public class BabySitterTest {
         BabySitter babySitter = new BabySitter();
         assertEquals("12", babySitter.baby(1));
     }
+
+    @Test
+    public void whenBabySitterIsPassedATwoHourShiftItReturnsTwentyFour() {
+        BabySitter babySitter = new BabySitter();
+        assertEquals("24", babySitter.baby(2));
+    }
+
+    @Test
+    public void whenBabySitterIsPassedAThreeHourShiftItReturnsThirtySix() {
+        BabySitter babySitter = new BabySitter();
+        assertEquals("36", babySitter.baby(3));
+    }
+
+    @Test
+    public void whenBabySitterIsPassedAFourHourShiftItReturnsFortyEight() {
+        BabySitter babySitter = new BabySitter();
+        assertEquals("48", babySitter.baby(4));
+    }
+
 }
