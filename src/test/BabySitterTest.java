f04f9fc13c9c765f6aaf9a7db9ae0bec341ce164
admin <admin@tapartnersllc.net>
2017-08-24 22:22:48 -0400

    refactored class instantiation

--- src/test/BabySitterTest.java
+++ src/test/BabySitterTest.java
@@ -1,30 +1,35 @@
 import com.mark.BabySitter;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class BabySitterTest {
+
+    BabySitter babySitter;
+
+    @Before
+    public void setUp() {
+        babySitter = new BabySitter();
+    }
+
     @Test
     public void whenBabySitterIsPassedAOneHourShiftItReturnsTwelve() {
-        BabySitter babySitter = new BabySitter();
         assertEquals("12", babySitter.baby(1));
     }
 
     @Test
     public void whenBabySitterIsPassedATwoHourShiftItReturnsTwentyFour() {
-        BabySitter babySitter = new BabySitter();
         assertEquals("24", babySitter.baby(2));
     }
 
     @Test
     public void whenBabySitterIsPassedAThreeHourShiftItReturnsThirtySix() {
-        BabySitter babySitter = new BabySitter();
         assertEquals("36", babySitter.baby(3));
     }
 
     @Test
     public void whenBabySitterIsPassedAFourHourShiftItReturnsFortyEight() {
-        BabySitter babySitter = new BabySitter();
         assertEquals("48", babySitter.baby(4));
     }
 
